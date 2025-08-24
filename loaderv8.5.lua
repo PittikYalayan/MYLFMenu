f4815a4ce2bb8ccd0322486bafbbb6599d50e692
PittikYalayan <ebulfezablak@gmail.com>
2025-08-24 16:10:36 +0300

    Update loaderv8.5.lua

--- loaderv8.5.lua
+++ loaderv8.5.lua
@@ -66,7 +66,7 @@ do
     local g = Tabs.Teleport:AddLeftGroupbox("Teleport")
     bindToggle(g, "tpkey", "Teleport (T Key)", features.ToggleTeleport)
     bindToggle(g, "autoBehind", "⚡ Always Behind Enemy", features.ToggleAutoBehind)
-    bindToggle(combatGroup, "autoTP", "⚡ Auto Farm Enemy", features.ToggleAutoTeleport)
+    bindToggle(g, "autoTP", "⚡ Auto Farm Enemy", features.ToggleAutoTeleport)
 
 
 
