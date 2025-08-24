98b2f2ce7eb9118403755a53e5e4731b513c85e1
PittikYalayan <ebulfezablak@gmail.com>
2025-08-25 01:38:41 +0300

    Update loaderv8.1.9.lua

--- loaderv8.1.9.lua
+++ loaderv8.1.9.lua
@@ -56,7 +56,8 @@ do
     bindToggle(g, "speed",     "Speed Boost (50)", features.ToggleSpeed)
     bindToggle(g, "fly",       "Fly (LCtrl down)", features.ToggleFly)
     bindToggle(g, "infjump",   "Infinite Jump",    features.ToggleInfiniteJump)
-   bindToggle(g, "godmode", "💀 Godmode", features.ToggleGodmode)
+    bindToggle(g, "godmode", "💀 Godmode", features.ToggleGodmode)
+    bindToggle(g, "tinyHitbox", "🛡️ Tiny Hitbox (Hard)", features.ToggleTinyHitbox)
     bindToggle(g, "hardInvis", "👻 Hard Invisible", features.ToggleHardInvisible)
     bindToggle(g, "noclip", "NoClip", features.ToggleNoclip)
 end
