f398d03227a8962b946908b5880a3b10e509fdbc
PittikYalayan <ebulfezablak@gmail.com>
2025-08-29 02:00:21 +0300

    Update meme1.7.lua

--- meme1.7.lua
+++ meme1.7.lua
@@ -508,43 +508,43 @@ do
 
     -- (İlerde: TeamCheck, Distance, Tracer, Healthbar vs fonksiyonların varsa buraya aynı pattern ile bağla)
 Controls.Toggle(left, "Enable ESP", false, function(on)
-        if  qfeatures.ToggleESP then features.ToggleESP(on) end
+        if  qfeatures.ToggleESP then qfeatures.ToggleESP(on) end
     end)
 
     Controls.Toggle(left, "Name ESP", false, function(on)
-        if  qfeatures.ToggleNameESP then features.ToggleNameESP(on) end
+        if  qfeatures.ToggleNameESP then qfeatures.ToggleNameESP(on) end
     end)
 
     Controls.Toggle(left, "Box ESP", false, function(on)
-        if  qfeatures.ToggleBoxESP then features.ToggleBoxESP(on) end
+        if  qfeatures.ToggleBoxESP then qfeatures.ToggleBoxESP(on) end
     end)
 
     Controls.Toggle(left, "Tracer ESP", false, function(on)
-        if  qfeatures.ToggleTracerESP then features.ToggleTracerESP(on) end
+        if  qfeatures.ToggleTracerESP then qfeatures.ToggleTracerESP(on) end
     end)
 
     Controls.Toggle(left, "Health Bar", false, function(on)
-        if  qfeatures.ToggleHealthESP then features.ToggleHealthESP(on) end
+        if  qfeatures.ToggleHealthESP then qfeatures.ToggleHealthESP(on) end
     end)
 
     Controls.Toggle(left, "Distance ESP", false, function(on)
-        if  qfeatures.ToggleDistanceESP then features.ToggleDistanceESP(on) end
+        if  qfeatures.ToggleDistanceESP then qfeatures.ToggleDistanceESP(on) end
     end)
 
     Controls.Toggle(right, "Skeleton ESP", false, function(on)
-        if  qfeatures.ToggleSkeletonESP then features.ToggleSkeletonESP(on) end
+        if  qfeatures.ToggleSkeletonESP then qfeatures.ToggleSkeletonESP(on) end
     end)
 
     Controls.Toggle(right, "Glow ESP", false, function(on)
-        if  qfeatures.ToggleGlowESP then features.ToggleGlowESP(on) end
+        if  qfeatures.ToggleGlowESP then qfeatures.ToggleGlowESP(on) end
     end)
 
     Controls.Toggle(right, "Corner Box", false, function(on)
-        if  qfeatures.ToggleCornerESP then features.ToggleCornerESP(on) end
+        if  qfeatures.ToggleCornerESP then qfeatures.ToggleCornerESP(on) end
     end)
 
     Controls.Toggle(right, "Offscreen Arrows", false, function(on)
-        if  qfeatures.ToggleOffscreenESP then features.ToggleOffscreenESP(on) end
+        if  qfeatures.ToggleOffscreenESP then qfeatures.ToggleOffscreenESP(on) end
     end)
 
     Controls.Toggle(right, "🎯 Enemy Big Hitbox", false, function(on)
