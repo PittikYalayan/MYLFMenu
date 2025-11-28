440c99c4f2a5171dec0e22762bb692f04891833f
PittikYalayan <ebulfezablak@gmail.com>
2025-11-28 14:49:56 +0300

    Update CENESENSEPREMIUM.lua

--- CENESENSEPREMIUM.lua
+++ CENESENSEPREMIUM.lua
@@ -441,7 +441,7 @@ local function createESP(plr)
     Name.Size = 16
     Name.Center = true
     Name.Outline = true
-    Name.Font = Enum.Font.SourceSansBold
+    Name.Font = Drawing.Fonts.UI
     Name.Color = Color3.new(1,1,1)
     Name.Transparency = 1
     local HealthBG = Drawing.new("Square")
