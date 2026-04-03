4c5dd08914bb3d06b34d6b23e8c6113f0adc3d72
PittikYalayan <ebulfezablak@gmail.com>
2026-04-03 18:50:23 +0300

    Create newmenufeatures.lua

---
+++ newmenufeatures.lua
@@ -0,0 +1,14 @@
+
+local RunService = game:GetService("RunService")
+local UIS        = game:GetService("UserInputService")
+local Workspace = game:GetService("Workspace")
+local Camera    = Workspace.CurrentCamera
+local Players    = game:GetService("Players")
+local Player     = Players.LocalPlayer
+local RS = game:GetService("ReplicatedStorage")
+
+local features = {}
+
+
+
+return features
